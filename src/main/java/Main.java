e85d6c5996abd3879350cddc07d1841bfde6fefd
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-19 11:54:35 -0400

    Finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
-import java.util.function.IntUnaryOperator;
+
+/* 
+ * Problem
+ * x Write a lambda expression that takes an integer and returns the next odd number. 
+ *      The expression should be public and static.
+ */
 
 public class Main {
-	// write the nextOddNum here
-    
+
+    // public static final NumberTransform nextOddNum = new NumberTransform() {
+    // public int applyAsInt(int number) {
+    // return (number % 2 != 0) ? number + 2 : number + 1;
+    // };
+    // };
+
+    public static final NumberTransform nextOddNum = (number) -> {
+        return (number % 2 != 0) ? number + 2 : number + 1;
+    };
+
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
@@ -10,4 +24,8 @@ public class Main {
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
     }
+}
+
+interface NumberTransform {
+    int applyAsInt(int number);
 }
